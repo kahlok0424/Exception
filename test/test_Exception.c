c6d2a1bfe36130a1d381aab43705d6ce81c21ac1
kahlok0424 <kahlok0424@gmail.com>
2018-06-28 11:58:54 +0800

    exception demo

--- test/test_Exception.c
+++ test/test_Exception.c
@@ -1,5 +1,8 @@
 #include "unity.h"
 #include "Exception.h"
+#include "CException.h"
+
+#define ERR_OUT_OF_BOUND    3
 
 void setUp(void)
 {
@@ -9,7 +12,57 @@ void tearDown(void)
 {
 }
 
-void test_Exception_NeedToImplement(void)
+char *names[]={
+  "Ali",
+  "David",
+  "Zhe Hao",
+  "Jane"
+};
+
+char *namesInIntel[] = {
+  "Alvin",
+  "Botak",
+  "Cat",
+  "Desmond"
+};
+
+char *namesInNXP[] = {
+  "Edmund",
+  "Fiona",
+  "Gx",
+  "Hanson"
+};
+
+char *getName(int index){
+  if(index >= 4)
+    //Throw(ERR_OUT_OF_BOUND);
+    return names[index];
+}
+
+void test_Exception_Given_1_Expect_error_code(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement Exception");
+    CEXCEPTION_T  e;
+    char *name;
+
+    Try{
+      name = getName(1);
+      //printf("Name is : %s",name);
+      TEST_ASSERT_EQUAL_STRING("David",name);
+    }Catch(e){
+      printf("Error code : %d",e);
+    }
+}
+
+void test_Exception_Given_1_Expect_Print_error_message(void)
+{
+    CEXCEPTION_T  e;
+    char *name;
+
+    Try{
+      name = getName(4);
+      printf("Name is : ");
+      TEST_FAIL_MESSAGE("Exceptied ERR_OUT_OF_BOUND to be thrown ,but none");
+    }Catch(e){
+      //printf("Error code : %d",e);
+    }
 }
